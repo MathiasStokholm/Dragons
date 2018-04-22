1e466be730a84e7ae98662899b31fc111f061508
Mathias Bøgh Stokholm <mathias.stokholm@gmail.com>
2018-04-22 15:18:12 +0200

    Added dungeon master image to landing page

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import {withRouter} from 'react-router-dom';
 import styled from 'styled-components';
+import dungeonMaster from '../data/dungeon_master.png';
 
 const AlignedText = styled.h1`
   text-align: center;
+  vertical-align: middle;
 `;
 
 class LandingPage extends React.Component {
     render() {
         return (
-            <div>
-                <AlignedText>2770 Dungeons</AlignedText>
-            </div>
+            <AlignedText>
+                2770 Dungeons
+                <br/>
+                <img style={{maxWidth: "100%"}} src={dungeonMaster}/>
+            </AlignedText>
         );
     }
 }
