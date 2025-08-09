d09798a4f7087358f92cd5b960c6fe3558ed4dd7
Mathias Bøgh Stokholm <mathias.stokholm@gmail.com>
2025-08-10 00:17:47 +0200

    Don't recreate DB and use proper URL for backend

--- backend/app/database.py
+++ backend/app/database.py
@@ -15,21 +15,19 @@ Base.query = db.session.query_property()
 
 
 def init_db():
-    # Uncomment to write fixtures to DB
-    from .models import Character, Encounter
-    
-    with app.app_context():
-        Base.metadata.create_all(bind=db.engine)
+    pass
 
-        for character in [Character(name='char_{}'.format(i), initiative=i, secret_initiative=False) for i in range(1, 20, 4)]:
-          db.session.add(character)
-          db.session.commit()
-        
-        characters = db.session.query(Character).order_by(Character.initiative.desc()).all()
-        encounter = Encounter(characters=characters, running=False, current_character_id=characters[0].id)
-        db.session.add(encounter)
-        db.session.commit()
-        
-        #encounter = Encounter(characters=[], running=False)
-        #db.session.add(encounter)
-        #db.session.commit()
+    # Uncomment to write fixtures to DB
+    #from .models import Character, Encounter
+    #
+    #with app.app_context():
+    #    Base.metadata.create_all(bind=db.engine)
+    #
+    #    for character in [Character(name='char_{}'.format(i), initiative=i, secret_initiative=False) for i in range(1, 20, 4)]:
+    #      db.session.add(character)
+    #      db.session.commit()
+    #
+    #    characters = db.session.query(Character).order_by(Character.initiative.desc()).all()
+    #    encounter = Encounter(characters=characters, running=False, current_character_id=characters[0].id)
+    #    db.session.add(encounter)
+    #    db.session.commit()
